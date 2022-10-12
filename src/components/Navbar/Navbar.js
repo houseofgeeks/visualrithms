ee685ddf112dd8ebe1011ce913402933c351aa31
CHIRAG <chiraggoel138@gmail.com>
2022-10-12 20:29:11 +0530

    added collapsable menu in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,26 +4,31 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      <div className="container">
-        <div className="title">
-          <Link to="/">Visualrithms</Link>
+    <div>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid text-bg-warning p-3">
+          <a class="navbar-brand text-primary fs-3" href="/">Visualrithms</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle fs-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Applications
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="/linearsearch">Linear Search</a></li>
+                  <li><a class="dropdown-item" href="/binarysearch">Binary Search</a></li>
+                  <li><a class="dropdown-item" href="/sorting">Sorting</a></li>
+                  <li><a class="dropdown-item" href="/breadthfirstsearch">Breadth First Search</a></li>
+                </ul>
+              </li>
+            </ul>
+
+          </div>
         </div>
-        <div className="navItems">
-          <Link to="/linearsearch" className="linear">
-            LinearSearch
-          </Link>
-          <Link to="/binarysearch" className="nav-link">
-            BinarySearch
-          </Link>
-          <Link to="/sorting" className="nav-link">
-            Sorting
-          </Link>
-          <Link to="/breadthfirstsearch" className="nav-link">
-            BreadthFirstSearch
-          </Link>
-        </div>
-      </div>
+      </nav>
     </div>
   );
 }
